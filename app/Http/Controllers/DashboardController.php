53ff87ba45fcf424a3dc29b3a5f18f656171cf69
Romain DI CANDIDO <116581960+RomainDC73@users.noreply.github.com>
2024-10-03 10:02:36 +0200

    laravel-lang installation

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Log as LogFacade;
 
 class DashboardController extends Controller
 {
@@ -12,8 +13,11 @@ class DashboardController extends Controller
      *
      * @return \Inertia\Response
      */
+
     public function index()
     {
+        // app()->setLocale('fr');
+        LogFacade::info('Locale actuelle : ' . app()->getLocale());
         return Inertia::render('Dashboard', [
             'greetings' => __('greetings'),
         ]);
