ed0033448c10be4496c57a6d70a00e798e020360
Romain DI CANDIDO <116581960+RomainDC73@users.noreply.github.com>
2024-10-08 16:11:01 +0200

    button see all on dashboard

--- resources/js/Components/PrimaryButton.jsx
+++ resources/js/Components/PrimaryButton.jsx
@@ -1,18 +1,35 @@
+import { Link } from '@inertiajs/react'; // Assurez-vous d'importer Link si vous utilisez Inertia.js
+
 export default function PrimaryButton({
     className = '',
     disabled,
     children,
+    href, // Ajout de la prop href
     ...props
 }) {
+    const buttonClass = `inline-flex items-center rounded-md border border-transparent bg-innerlightblue px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-innerdarkblue focus:outline-none focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900 ${
+        disabled ? 'opacity-25' : ''
+    } ${className}`;
+
+    // Si href est fourni, retourner un lien
+    if (href) {
+        return (
+            <Link
+                href={href}
+                className={buttonClass}
+                {...props}
+            >
+                {children}
+            </Link>
+        );
+    }
+
+    // Sinon, retourner un bouton
     return (
         <button
-            {...props}
-            className={
-                `inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900 ${
-                    disabled && 'opacity-25'
-                } ` + className
-            }
+            className={buttonClass}
             disabled={disabled}
+            {...props}
         >
             {children}
         </button>
