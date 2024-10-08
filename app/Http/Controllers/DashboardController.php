2808a6b78c81c1940b373b51a29b09715246c77d
Romain DI CANDIDO <116581960+RomainDC73@users.noreply.github.com>
2024-10-08 15:11:25 +0200

    indentation

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -17,7 +17,7 @@ class DashboardController extends Controller
 
     public function index()
     {
-        $posts = Post::where('user_id', Auth::id())->with('mood')->orderBy('created_at', 'desc')->get();
+        $posts = Post::where('user_id', Auth::id())->with('mood')->orderBy('created_at', 'desc', )->get();
 
         return Inertia::render('Dashboard', [
             'greetings' => __('greetings'),
