052f50c3df45274d90cb657a3274b540f241bfcd
Romain DI CANDIDO <116581960+RomainDC73@users.noreply.github.com>
2024-10-08 10:37:03 +0200

    date added to postslist

--- resources/js/Components/PostsList.jsx
+++ resources/js/Components/PostsList.jsx
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from '@inertiajs/react';
-import { usePage } from '@inertiajs/react';
-
-
 
 const PostsList = ({ posts = [] }) => {  // Ajoute une valeur par défaut pour 'posts'
+
+    const formattedDate = (dateString) => {
+        const date = new Date(dateString);
+        return date.toLocaleDateString('fr-FR', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+        });
+    };
     return (
         <div>
             <h1>Mes Posts</h1>
@@ -13,6 +21,7 @@ const PostsList = ({ posts = [] }) => {  // Ajoute une valeur par défaut pour '
                     posts.map(post => (
                         <li key={post.id}>
                             <h3>{post.mood.translated_name}</h3> {/* Affiche le nom de l'humeur */}
+                            <p className="text-sm text-gray-500">{formattedDate(post.created_at)}</p>
                             <p>{post.description}</p> {/* Affiche la description */}
                            {/* Lien vers la page de détail du post */}
                            <Link href={`/post/${post.id}`} className="text-blue-500 underline">
@@ -21,7 +30,7 @@ const PostsList = ({ posts = [] }) => {  // Ajoute une valeur par défaut pour '
                         </li>
                     ))
                 ) : (
-                    <p>Aucun post trouvé.</p>  // Message quand il n'y a pas de posts
+                    <p>Vous n'avez publié aucun post.</p>  // Message quand il n'y a pas de posts
                 )}
             </ul>
         </div>
