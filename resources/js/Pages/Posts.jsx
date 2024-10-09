db025782d244c6c4f80241208499ad65dee70e46
Romain DI CANDIDO <116581960+RomainDC73@users.noreply.github.com>
2024-10-09 11:20:04 +0200

    posts page

--- resources/js/Pages/Posts.jsx
+++ resources/js/Pages/Posts.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import PostsList from '@/Components/PostsList';
-const Posts = () => {
-    return (
-        <div>
-            <h1>Mes posts</h1>
-            <PostsList />
-        </div>
-    );
-};
+import { Head, usePage } from '@inertiajs/react';
 
-export default Posts;
+
+export default function Posts() {
+    // Récupère l'utilisateur, les salutations, et les posts passés depuis le contrôleur
+    const { auth, posts } = usePage().props;
+
+
+    return (
+        <AuthenticatedLayout>
+            <Head title="Posts" />
+            <div>
+                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                    {/* Ajout du composant PostsList pour afficher les posts de l'utilisateur */}
+                    <div className="mt-2 overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                        <div className="p-6">
+                            <PostsList posts={posts} /> {/* Passe les posts récupérés */}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    );
+}
