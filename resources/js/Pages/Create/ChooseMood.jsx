270491ec3707cacc682c5a75f82736e27c86a809
Romain DI CANDIDO <116581960+RomainDC73@users.noreply.github.com>
2024-10-10 11:12:45 +0200

    next button canceled, step choose mood ok

--- resources/js/Pages/Create/ChooseMood.jsx
+++ resources/js/Pages/Create/ChooseMood.jsx
@@ -3,16 +3,12 @@ import { Head, useForm } from '@inertiajs/react';
 import MoodCard from '@/Components/MoodCard';
 
 export default function ChooseMood({ moods, moodTranslations }) {
-    const { data, setData, post } = useForm({
+    const { setData, post } = useForm({
         mood_id: '',
     });
 
     const handleMoodSelect = (id) => {
         setData('mood_id', id);
-    };
-
-    const submitMood = (e) => {
-        e.preventDefault();
         post('/create/choose-action');
     };
 
@@ -26,24 +22,15 @@ export default function ChooseMood({ moods, moodTranslations }) {
         >
             <Head title="Choisis ton mood" />
             <div className="flex flex-col items-center">
-                <form onSubmit={submitMood}>
-                    <div className="grid grid-cols-1 md:grid-cols-3 gap-4"> {/* Affichage en grille avec 3 colonnes sur grand écran */}
-                        {moods.map(mood => (
-                            <div key={mood.id} onClick={() => handleMoodSelect(mood.id)}>
-                                <MoodCard mood={mood} moodTranslations={moodTranslations} />
-                            </div>
-                        ))}
-                    </div>
-                    <div className="mt-6 text-center">
-                        <button
-                            type="submit"
-                            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 disabled:opacity-50"
-                            disabled={!data.mood_id}
-                        >
-                            Next
-                        </button>
-                    </div>
-                </form>
+                {/* Formulaire */}
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                    {moods.map(mood => (
+                        <div key={mood.id} onClick={() => handleMoodSelect(mood.id)}>
+                            {/* Passe moodTranslations ici */}
+                            <MoodCard mood={mood} moodTranslations={moodTranslations} />
+                        </div>
+                    ))}
+                </div>
             </div>
         </AuthenticatedLayout>
     );
