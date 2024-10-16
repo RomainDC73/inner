0707eb8b4a92f1a33318aec1db0248bfdc6095ae
Romain DI CANDIDO <116581960+RomainDC73@users.noreply.github.com>
2024-10-16 14:19:44 +0200

    ImagePreview component created

---
+++ resources/js/Components/ImagePreview.jsx
@@ -0,0 +1,16 @@
+export default function ImagePreview({ src, onRemove }) {
+    return (
+        <div className="relative mt-4">
+            <img src={src} alt="Prévisualisation" className="w-32 h-32 object-cover rounded-md" />
+            {onRemove && (
+                <button
+                    type="button"
+                    onClick={onRemove}
+                    className="absolute top-0 right-0 bg-red-600 text-white rounded-full p-1"
+                >
+                    🗑
+                </button>
+            )}
+        </div>
+    );
+}
