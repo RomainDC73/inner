eef29973b7cc8c7ddf96f519abd62ceab05ae393
Romain DI CANDIDO <116581960+RomainDC73@users.noreply.github.com>
2024-10-21 11:22:28 +0200

    post page style

--- resources/js/Pages/Post.jsx
+++ resources/js/Pages/Post.jsx
@@ -44,7 +44,7 @@ export default function PostShow() {
         >
             <Head title={postTitle} />
 
-            <div className="py-12">
+            <div>
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900 space-y-3">
