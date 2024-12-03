1bcf0a9e5952f222aa43b60300df086a1508a313
Romain DI CANDIDO <116581960+RomainDC73@users.noreply.github.com>
2024-12-03 15:23:32 +0100

    emojison greeting phrases

--- lang/fr/greetings.php
+++ lang/fr/greetings.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'greetings_01' => 'Comment ça va aujourd\'hui ?',
-    'greetings_02' => 'J\'espère que tu vas bien.',
-    'greetings_03' => 'Comment se passe cette journée ?',
-    'greetings_04' => 'Comment te sens-tu ?',
+    'greetings_01' => 'Comment ça va aujourd\'hui ? 👋',
+    'greetings_02' => 'J\'espère que tu vas bien. 🤗',
+    'greetings_03' => 'Comment se passe cette journée ? 🌤',
+    'greetings_04' => 'Comment te sens-tu ? 😊',
 ];
