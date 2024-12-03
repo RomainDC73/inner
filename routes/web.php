78b014f2a977b6d7a20362c840653fd23a505514
Romain DI CANDIDO <116581960+RomainDC73@users.noreply.github.com>
2024-12-03 17:04:09 +0100

    App Logo and link on header

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
-});
+})->name('welcome');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
