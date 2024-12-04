44cc9f252a8a7b3517b312a6a3b46761f62fb434
Romain DI CANDIDO <116581960+RomainDC73@users.noreply.github.com>
2024-12-04 11:03:42 +0100

    orb clouds on welcome page

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,4 +1,7 @@
 import { Head, Link } from '@inertiajs/react';
+import { MdAccountCircle } from "react-icons/md";
+import Dropdown from '@/Components/Dropdown';
+import '../../css/cloud.css';
 import ApplicationLogo from '@/Components/ApplicationLogo';
 
 export default function Welcome({ auth, laravelVersion, phpVersion }) {
@@ -16,20 +19,24 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
     return (
         <>
             <Head title="Welcome" />
-            <div className="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
+            <div className="orbContainer">
+                <div className="orb"></div>
+                <div className="orb"></div>
+                <div className="orb"></div>
+            </div>
+            <div className="text-black/50 dark:text-white/50">
                 <div className="relative flex min-h-screen flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
                     <div className="relative w-full max-w-2xl px-6 lg:max-w-7xl">
                         <header className="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
+
                             <div className="flex lg:col-start-2 lg:justify-center">
-                            <Link href={route('welcome')}>
-                                    <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
-                            </Link>
+
                             </div>
                             <nav className="-mx-3 flex flex-1 justify-end">
                                 {auth.user ? (
                                     <Link
                                         href={route('dashboard')}
-                                        className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                        className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
                                     >
                                         Dashboard
                                     </Link>
@@ -37,19 +44,23 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                                     <>
                                         <Link
                                             href={route('login')}
-                                            className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                            className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
                                         >
-                                            Log in
+                                            Connexion
                                         </Link>
                                         <Link
                                             href={route('register')}
-                                            className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                            className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
                                         >
-                                            Register
+                                            Enregistrement
                                         </Link>
                                     </>
                                 )}
                             </nav>
+
+                            <Link href={route('welcome')}>
+                                    <ApplicationLogo className="block h-9 w-auto fill-current" />
+                            </Link>
                         </header>
 
                         <main className="mt-6">
@@ -181,14 +192,22 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                                     </svg>
                                 </a>
                             </div>
+
                         </main>
 
-                        <footer className="py-16 text-center text-sm text-black dark:text-white/70">
+                        <footer className="py-16 text-center text-sm text-black">
                             Laravel v{laravelVersion} (PHP v{phpVersion})
+                            <div className="orbContainer-bottom">
+                <div className="orb-bottom"></div>
+                <div className="orb-bottom"></div>
+                <div className="orb-bottom"></div>
+            </div>
                         </footer>
+
                     </div>
                 </div>
             </div>
         </>
+
     );
 }
