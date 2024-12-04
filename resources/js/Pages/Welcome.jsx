9d0b3b7ac6f43e2d0403f6244773ecd88d294c82
Romain DI CANDIDO <116581960+RomainDC73@users.noreply.github.com>
2024-12-04 14:57:59 +0100

    dropdown to connect, logo in center

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -24,46 +24,59 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                 <div className="orb"></div>
                 <div className="orb"></div>
             </div>
-            <div className="text-black/50 dark:text-white/50">
-                <div className="relative flex min-h-screen flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
-                    <div className="relative w-full max-w-2xl px-6 lg:max-w-7xl">
-                        <header className="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
-
-                            <div className="flex lg:col-start-2 lg:justify-center">
-
+            <div className="text-black/50">
+                <div className="text-white relative flex min-h-screen flex-col items-center justify-center">
+                    <div className="relative w-full max-w-2xl px-6 lg:max-w-7xl ">
+                        <header className="gap-2 py-6 lg:grid-cols-3">
+                            <div className="flex justify-end">
+                                <Dropdown>
+                                    <Dropdown.Trigger>
+                                        <span className="inline-flex rounded-md">
+                                            <button
+                                                type="button"
+                                                className="inline-flex items-center rounded-md border border-transparent px-3 py-2 text-sm font-medium leading-4 text-innerdarkblue transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none"
+                                            >
+                                                <MdAccountCircle size="28" />
+                                            </button>
+                                        </span>
+                                    </Dropdown.Trigger>
+                                    <Dropdown.Content>
+                                        <div>
+                                            {auth.user ? (
+                                                <Dropdown.Link
+                                                    href={route('dashboard')}
+                                                    className="rounded-md px-3 py-2 transition"
+                                                >
+                                                    Dashboard
+                                                </Dropdown.Link>
+                                            ) : (
+                                                <>
+                                                    <Dropdown.Link
+                                                        href={route('login')}
+                                                        className="rounded-md px-3 py-2 transition"
+                                                    >
+                                                        Connexion
+                                                    </Dropdown.Link>
+                                                </>
+                                            )}
+                                        </div>
+                                    </Dropdown.Content>
+                                </Dropdown>
                             </div>
-                            <nav className="-mx-3 flex flex-1 justify-end">
-                                {auth.user ? (
-                                    <Link
-                                        href={route('dashboard')}
-                                        className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
-                                    >
-                                        Dashboard
-                                    </Link>
-                                ) : (
-                                    <>
-                                        <Link
-                                            href={route('login')}
-                                            className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
-                                        >
-                                            Connexion
-                                        </Link>
-                                        <Link
-                                            href={route('register')}
-                                            className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
-                                        >
-                                            Enregistrement
-                                        </Link>
-                                    </>
-                                )}
-                            </nav>
-
-                            <Link href={route('welcome')}>
-                                    <ApplicationLogo className="block h-9 w-auto fill-current" />
-                            </Link>
+                            <div className="flex items-center justify-center w-full mt-6">
+                                <Link href={route('welcome')}>
+                                    <ApplicationLogo className="h-14" />
+                                </Link>
+                            </div>
                         </header>
 
                         <main className="mt-6">
+                            <Link
+                                href={route('register')}
+                                className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
+                            >
+                                Enregistrement
+                            </Link>
                             <div className="grid gap-6 lg:grid-cols-2 lg:gap-8">
                                 <a
                                     href="https://laravel.com/docs"
@@ -192,22 +205,19 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                                     </svg>
                                 </a>
                             </div>
-
                         </main>
 
                         <footer className="py-16 text-center text-sm text-black">
                             Laravel v{laravelVersion} (PHP v{phpVersion})
                             <div className="orbContainer-bottom">
-                <div className="orb-bottom"></div>
-                <div className="orb-bottom"></div>
-                <div className="orb-bottom"></div>
-            </div>
+                                <div className="orb-bottom"></div>
+                                <div className="orb-bottom"></div>
+                                <div className="orb-bottom"></div>
+                            </div>
                         </footer>
-
                     </div>
                 </div>
             </div>
         </>
-
     );
 }
