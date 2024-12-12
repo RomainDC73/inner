7951a1988e56e5ce1ade2e88fba06942dc97b908
Romain DI CANDIDO <116581960+RomainDC73@users.noreply.github.com>
2024-12-12 10:48:41 +0100

    registration blade view created

---
+++ resources/views/emails/registration.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h2>Bienvenue [name]</h2>
+    <p>Vous venez de créer votre compte sur l'application Inner et nous vous en remercions !</p>
+    <ul>
+      <li><strong>Nom</strong> : {{ $contact['nom'] }}</li>
+    </ul>
+  </body>
+</html>
