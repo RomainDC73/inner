117699ae3bf98d77a7bf85c4f23aa126b58a21d1
Romain DI CANDIDO <116581960+RomainDC73@users.noreply.github.com>
2024-12-16 11:55:09 +0100

    Fix SSL configuration for production

--- vite.config.js
+++ vite.config.js
@@ -3,25 +3,32 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 import fs from 'fs';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.jsx',
-            refresh: true,
-        }),
-        react(),
-    ],
-    server: {
-        https: {
-            key: fs.readFileSync('/Users/fantaz/.config/valet/Certificates/inner.test.key'),
-            cert: fs.readFileSync('/Users/fantaz/.config/valet/Certificates/inner.test.crt'),
+export default defineConfig(({ mode }) => {
+    const isLocal = mode === 'development';
+
+    return {
+        plugins: [
+            laravel({
+                input: 'resources/js/app.jsx',
+                refresh: true,
+            }),
+            react(),
+        ],
+        server: {
+            https: isLocal
+                ? {
+                      key: fs.readFileSync('/Users/fantaz/.config/valet/Certificates/inner.test.key'),
+                      cert: fs.readFileSync('/Users/fantaz/.config/valet/Certificates/inner.test.crt'),
+                  }
+                : undefined,
+
+            host: 'localhost',
+            port: 5173,
+            cors: true,
+            watch: {
+                usePolling: true,
+                interval: 100,
+            },
         },
-        host: 'localhost',
-        port: 5173,
-        cors: true,
-        watch: {
-            usePolling: true,
-            interval: 100,
-        },
-    },
+    };
 });
