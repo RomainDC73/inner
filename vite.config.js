76ea268e24864a49c881a888444f5676f2b31e36
Romain DI CANDIDO <116581960+RomainDC73@users.noreply.github.com>
2024-12-16 12:49:03 +0100

    vite package modified

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,22 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
-import fs from 'fs';
 
-export default defineConfig(({ mode }) => {
-    const isLocal = mode === 'development';
-
-    return {
-        plugins: [
-            laravel({
-                input: 'resources/js/app.jsx',
-                refresh: true,
-            }),
-            react(),
-        ],
-        server: {
-            // Si en développement, utilise les certificats locaux
-            https: isLocal
-                ? {
-                      key: fs.readFileSync('/Users/fantaz/.config/valet/Certificates/inner.test.key'),
-                      cert: fs.readFileSync('/Users/fantaz/.config/valet/Certificates/inner.test.crt'),
-                  }
-                : undefined, // En production, ignore les certificats locaux
-
-            host: 'localhost', // L'adresse d'écoute en local (localhost)
-            port: 5173, // Le port d'écoute
-            cors: true, // Active CORS si nécessaire
-            watch: {
-                usePolling: true,
-                interval: 100,
-            },
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: 'resources/js/app.jsx',
+            refresh: true,
+        }),
+        react(),
+    ],
+    server: {
+        host: 'localhost',
+        port: 5173,
+        cors: true,
+        watch: {
+            usePolling: true,
+            interval: 100,
         },
-    };
+    },
 });
