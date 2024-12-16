143ef7df25b5e4fa1d941c73abab5aa4ca05e420
Romain DI CANDIDO <116581960+RomainDC73@users.noreply.github.com>
2024-12-16 13:28:01 +0100

    modif on vite config

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Favicon -->
         <link rel="icon" href="{{ asset('my-favicon.ico') }}" type="image/x-icon">
 
         <!-- Fonts -->
