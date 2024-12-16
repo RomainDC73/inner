b12a6d3b44545ed6a02f07a04704cec741ed4f9c
Romain DI CANDIDO <116581960+RomainDC73@users.noreply.github.com>
2024-12-16 13:10:28 +0100

    vite confit port

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
         react(),
     ],
     server: {
-        host: 'O.O.O.O',
+        host: '0.0.0.0',
         port: 5173,
         cors: true,
         watch: {
